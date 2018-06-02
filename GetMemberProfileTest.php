42ce56882ba92476c3d8eba1b3b2ddae3508f6a3
kreangk <kreangk@gmail.com>
2018-06-02 17:18:52 +0000

    Update GetMemberProfileTest.php

--- GetMemberProfileTest.php
+++ GetMemberProfileTest.php
@@ -1,12 +1,11 @@
 <?php
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('Kv3HpQDne5CosW0hTmIWNoBtT8CuZFUjeboZFoqzRc6Pth4RyV3+9ER7nJSDxh3Qt1LBAka+Fez0PXn7kZuZi5cN/WgNlZekEv8c4ip+RYIDajue++oxCW5nOC2OJOYH5IEVilyYer1/8u3Yc5znMQdB04t89/1O/w1cDnyilFU=', $textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
-
-// Failed
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+        $memberIds = [];
+        $continuationToken = null;
+        do {
+            $response = $this->getGroupMemberIds('C736d844d8deb0e75256f7fcf6b388ff1', $continuationToken);
+            $data = $response->getJSONDecodedBody();
+            $memberIds = array_merge($memberIds, $data['memberIds']);
+            $continuationToken = isset($data['next']) ? $data['next'] : null;
+        } while ($continuationToken);
     
